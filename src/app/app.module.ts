10fe48a759574bbd96486c94d2935c06ca945892
Amauri Souza <amauri2015gg@gmail.com>
2018-10-17 11:55:12 -0300

    FEATURE - create component login, dashboar and task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppHeaderComponent } from './layout/app-header/app-header.component';
 import { AppFooterComponent } from './layout/app-footer/app-footer.component';
 import { AppSidebarComponent } from './layout/app-sidebar/app-sidebar.component';
 import { MzButtonModule, MzNavbarModule, MzDropdownModule, MzSidenavModule} from 'ngx-materialize';
+import { TasksComponent } from './tasks/tasks.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
     AppFooterComponent,
-    AppSidebarComponent
+    AppSidebarComponent,
+    TasksComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
