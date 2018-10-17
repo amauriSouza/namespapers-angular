6e8045daa9cd5003c7a9ba5ab8d8c56fdb055e27
Amauri Souza <amauri2015gg@gmail.com>
2018-10-17 10:49:10 -0300

    FEATURE - create components header, footer and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './layout/app-header/app-header.component';
+import { AppFooterComponent } from './layout/app-footer/app-footer.component';
+import { AppSidebarComponent } from './layout/app-sidebar/app-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppSidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/layout/app-sidebar/app-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-sidebar',
+  templateUrl: './app-sidebar.component.html',
+  styleUrls: ['./app-sidebar.component.css']
+})
+export class AppSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
